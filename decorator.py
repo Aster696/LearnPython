d10620988c7dabb5cbdf59d69a11841eb52fd450
Aster696 <asterrebeiro24@gmail.com>
2026-09-06 08:02:48 +0530

    fir

---
+++ decorator.py
@@ -0,0 +1,16 @@
+def greater_first(func): 
+    def wrap(a, b):
+        if(a < b):
+            a,b = b,a
+        return func(a,b)
+    return wrap
+
+@greater_first
+def minus(a,b):
+    return a - b
+
+@greater_first
+def divide(a,b):
+    return a / b
+
+print(minus(1, 2))
