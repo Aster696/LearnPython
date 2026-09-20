e843a66b03a00ce286a97f90ef97d1b5413c9827
Aster696 <asterrebeiro24@gmail.com>
2026-09-20 21:47:27 +0530

    fist

--- DSA/siglyLinkedList2.py
+++ DSA/siglyLinkedList2.py
@@ -14,26 +14,50 @@ class LinkedList:
         self.head = node
 
     def appendAtEnd(self, data):
-        node = Node(data, self.head)
-        self.next = node
+        if(self.isEmpty()):
+            self.head = Node(data, None)
+            return
+        
+        itr = self.head
+        while itr.next:
+            itr = itr.next
+
+        itr.next = Node(data, None)
+
+    def isEmpty(self):
+        if self.head == None:
+            print("List is empty")
+        return self.head == None
 
     def print(self):
-
-        if self.head is None:
-            print('List is empty')
-            return;
+        print("---------------List---------------")
+        if(self.isEmpty()):
+            return
 
         itr = self.head
         while itr:
             print(itr.data)
             itr = itr.next
 
+    # take user input dynamically
+    def addData(self):
+        isYes = 'y'
+        while isYes[0].lower() == 'y':
+            value = input("Enter value: ")
+            self.appendAtBegning(value)
+
+            isYes = input("Add new value? press [n/y]: ")
+
+        self.print()
+
 
 if __name__ == '__main__':
     ll = LinkedList()
-    ll.appendAtBegning(4)
-    ll.appendAtBegning(6)
-    ll.appendAtBegning(2)
-    ll.appendAtEnd(7)
+    # ll.appendAtBegning(4)
+    # ll.appendAtBegning(6)
+    # ll.appendAtBegning(2)
+    # ll.appendAtEnd(7)
+    # ll.appendAtBegning(8)
 
-    ll.print()
+    # ll.print()
+    ll.addData()
